2816f5e4fda0d62892c0b3752f1415035054cb31
apriadi4425 <apripeni15@gmail.com>
2020-12-11 06:58:51 +0800

    mengubah tulisan copyright

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -5,11 +5,11 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a href="http://apriadi-94.surge,sh" target="_blank" rel="noopener noreferrer">@copyright Apriadi, S.Si</a>
+        <span className="ml-1">&copy; UpToTech.</span>
       </div>
       <div className="mfs-auto">
-        <span className="mr-1">Powered by</span>
+        <span className="mr-1">Terimakasih Untuk</span>
         <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI for React</a>
       </div>
     </CFooter>
